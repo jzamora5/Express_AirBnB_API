8f2cd816087af658cf5a240e9c3b032ab9174686
jzamora5 <jzamora_5@yahoo.com>
2021-02-05 14:52:27 -0500

    User Endpoints

--- routes/amenities.js
+++ routes/amenities.js
@@ -6,7 +6,7 @@ function amenityRouting(app) {
   const router = express.Router();
   app.use('/', router);
 
-  router.get('/amenity', async (req, res) => {
+  router.get('/amenities', async (req, res) => {
     let amenities;
     try {
       amenities = await Amenity.findAll();
@@ -16,6 +16,96 @@ function amenityRouting(app) {
     }
 
     res.status(200).send(amenities);
+  });
+
+  router.get('/amenities/:amenity_id', async (req, res) => {
+    const { amenity_id } = req.params;
+
+    let amenity;
+
+    try {
+      amenity = await Amenity.findByPk(amenity_id);
+    } catch (err) {
+      console.log(err.message);
+      return res.status(404).send('Not Found');
+    }
+
+    if (!amenity) return res.status(404).send('Not Found');
+
+    return res.status(200).send(amenity);
+  });
+
+  router.delete('/amenities/:amenity_id', async (req, res) => {
+    const { amenity_id } = req.params;
+
+    let amenity;
+
+    try {
+      amenity = await Amenity.findByPk(amenity_id);
+    } catch (err) {
+      console.log(err.message);
+      return res.status(404).send('Not Found');
+    }
+
+    if (!amenity) return res.status(404).send('Not Found');
+
+    try {
+      await amenity.destroy();
+    } catch (err) {
+      console.log(err.message);
+      return res.status(404).send('Could Not Delete');
+    }
+
+    return res.status(200).send();
+  });
+
+  router.post('/amenities', async (req, res) => {
+    const { body } = req;
+
+    if (body.name === undefined) {
+      return res.status(400).send('Missing name');
+    }
+
+    const amenity = Amenity.build(body);
+
+    try {
+      await amenity.save();
+    } catch (err) {
+      console.log(err.message);
+      return res.status(500).send('Could Not Create');
+    }
+
+    return res.status(200).send(amenity);
+  });
+
+  router.put('/amenities/:amenity_id', async (req, res) => {
+    const { amenity_id } = req.params;
+    const { body } = req;
+
+    let amenity;
+
+    try {
+      amenity = await Amenity.findByPk(amenity_id);
+    } catch (err) {
+      console.log(err.message);
+      return res.status(404).send('Not Found');
+    }
+
+    if (!amenity) return res.status(404).send('Not Found');
+
+    const ignore = ['id', 'createdAt', 'updatedAt'];
+
+    for (const [key, value] of Object.entries(body)) {
+      if (!ignore.includes(key)) amenity[key] = value;
+    }
+
+    try {
+      await amenity.save();
+    } catch (err) {
+      return res.status(500).send('Could Not Update');
+    }
+
+    return res.status(200).send(amenity);
   });
 }
 
